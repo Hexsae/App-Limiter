d6aa3a63b9993e75be4e7a4679aae66fbf038aae
Wright <hexsae@gmail.com>
2021-03-13 11:33:48 -0600

    More functionality

--- App.py
+++ App.py
@@ -19,6 +19,8 @@ class Main_UI(QMainWindow):
         self.data = {}
         self.item_locations = {} #item row
         self.threadpool = QThreadPool()
+        self.setWindowIcon(QIcon("okhand.png"))
+        self.finished = []
 
         #Initializing save file
         try:
@@ -36,7 +38,8 @@ class Main_UI(QMainWindow):
         self.deletebtn = self.findChild(QPushButton, "deletebtn")
         self.startbtn = self.findChild(QPushButton, "startbtn")
         self.stopbtn = self.findChild(QPushButton, "stopbtn")
-        self.close_appbtn = self.findChild(QPushButton, "close_appbtn")
+        self.helpbtn = self.findChild(QAction, "helpbtn")
+
         self.log_status = self.findChild(QLabel, "log_status")
 
         #Events
@@ -45,8 +48,10 @@ class Main_UI(QMainWindow):
         self.startbtn.clicked.connect(self.startbtn_clicked)
         self.stopbtn.clicked.connect(self.stopbtn_clicked)
         self.deletebtn.clicked.connect(self.deletebtn_clicked)
+        self.helpbtn.triggered.connect(self.helpbtn_clicked)
 
         self.addWindow = AddWindow(self)
+        self.helpWindow = HelpWindow()
         self.show()
 
         quit = QAction("Quit", self)
@@ -87,6 +92,9 @@ class Main_UI(QMainWindow):
     def item_selected(self):
         if self.running: return
         self.deletebtn.setEnabled(True)
+
+    def helpbtn_clicked(self):
+        self.helpWindow.show()
     
     def deletebtn_clicked(self):
         try:
@@ -108,25 +116,31 @@ class Main_UI(QMainWindow):
     
     def handle_progress(self):
         try:
-            processes = list(set(map(lambda x: x.name(), psutil.process_iter())))
+            proc_list = list(psutil.process_iter())
+            processes = list(set(map(lambda x: x.name().capitalize(), proc_list)))
+
             for item in self.data:
                 row = self.item_locations[item]
                 status = "Running..." if item in processes else "closed"
+                self.list_apps.setItem(row, 2, QTableWidgetItem(status))
 
-                if not self.data[item]["disabled"] and self.data[item]["time"] != 0:
+                if not self.data[item]["disabled"] and not item in self.finished:
                     self.data[item]["time"] -= 1
                     time = self.Tools.convert_time(self.data[item]["time"])
                     self.list_apps.setItem(row, 0, QTableWidgetItem(item))
                     self.list_apps.setItem(row, 1, QTableWidgetItem(time))
-                    self.list_apps.setItem(row, 2, QTableWidgetItem(status))
 
                     #Setting colors
                     if self.data[item]["time"] < 900:
-                        print(row)
                         self.list_apps.item(row, 1).setForeground(QColor(255, 0, 0))
-                    if status == "Running...":
-                        self.list_apps.item(row, 2).setForeground(QColor(0, 200, 0))
+                if status == "Running...":
+                    self.list_apps.item(row, 2).setForeground(QColor(0, 200, 0))
 
+                if self.data[item]["time"] == 0  and not item in self.finished:
+                    self.finished.append(item)
+                    box = QMessageBox.warning(self, "Time up!",
+                    "You have used {} for the set amount of time!".format(item, time), QMessageBox.Ok)
+                
                 self.update()
         except Exception as e:
             print(e)
@@ -137,6 +151,8 @@ class Main_UI(QMainWindow):
             self.startbtn.setDisabled(True)
             self.running = True
             self.worker = Worker(self.Tools.start_refresh)
+            #reset sefl.finished
+            self.finished = []
 
             self.worker.signals.progress.connect(self.handle_progress)
 
@@ -213,9 +229,15 @@ class AddWindow(QMainWindow):
     def load_processes(self):
         processes = list(set(map(lambda x: x.name(), psutil.process_iter())))
         processes = sorted(processes, key=str.casefold)
+        processes = [x.capitalize() for x in processes]
         processes.insert(0, "")
         self.open_apps.clear()
         self.open_apps.addItems(processes)
+
+class HelpWindow(QDialog):
+    def __init__(self):
+        super(HelpWindow, self).__init__()
+        uic.loadUi("ui/Help.ui", self)
 
 
 app = QApplication(sys.argv)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # App-Limiter
+Designed to help you limit how long you spend on an app!
+
+![Main window](https://cdn.discordapp.com/attachments/795052462355644426/820211045959598080/unknown.png)
